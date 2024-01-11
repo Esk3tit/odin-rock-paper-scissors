f90490b863f1c3cae3b8f1b2560e3df7b7b2c4c0
Khai Phan <khaiphn41@gmail.com>
2024-01-10 18:39:27 -0800

    Finish refactor to have UI to play game

--- main.js
+++ main.js
@@ -7,50 +7,128 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
-    // Infinite loop, if there is no tie then we return so we break out of the loop
-    // otherwise repeat loop until there is no tie
-    while (true) {
-        if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            return 'You win! Rock beats scissors.';
-        } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            return 'You lose! Paper beats rock.';
-        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            return 'You win! Paper beats rock.';
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            return 'You lose! Scissors beats paper.';
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            return 'You win! Scissors beats paper.';
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            return 'You lose! Rock beats scissors.';
-        } else {
-            // if there is a tie, replay the round
-            computerSelection = getComputerChoice();
-            playerSelection = prompt('Tie! Try again. Rock, paper, or scissors?');
-            playerSelection = playerSelection.toLowerCase();
-        }
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats scissors.';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper beats rock.';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats rock.';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beats paper.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats paper.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats scissors.';
+    } else {
+        return 'Tie! Play again.';
+    }
+}
+
+function onClickHandler(playerSelection, scores, playerScoreElem, computerScoreElem, resultsContainerElem, rockButton, paperButton, scissorsButton) {
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+
+    if (result.startsWith('You win')) {
+        scores.player++;
+    } else if (result.startsWith('You lose')) {
+        scores.computer++;
+    }
+
+    // Add new p to display result if it doesn't exist, otherwise update existing div
+    const resultElem = document.querySelector('p#result');
+    if (!resultElem) {
+        const newResultElem = document.createElement('p');
+        newResultElem.id = 'result';
+        newResultElem.textContent = result;
+        resultsContainerElem.appendChild(newResultElem);
+    } else {
+        resultElem.textContent = result;
+    }
+
+    playerScoreElem.textContent = `Player score: ${scores.player}`;
+    computerScoreElem.textContent = `Computer score: ${scores.computer}`;
+
+    // If either player or computer reaches 5 points, display winner and disable buttons
+    if (scores.player === 5 || scores.computer === 5) {
+        const winnerElem = document.createElement('p');
+        winnerElem.id = 'winner';
+        winnerElem.textContent = scores.player > scores.computer ? 'YOU\'RE WINNER!' : 'L BOZO';
+        resultsContainerElem.appendChild(winnerElem);
+
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
     }
 }
 
 function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
+    const scores = { player: 0, computer: 0 };
 
-    for(let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Rock, paper, or scissors?');
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
+    // Create three buttons for rock, paper, and scissors
+    // create it w/ dom methods, set text content, add event listeners for click to call playRound, append to body
+    const rockButton = document.querySelector('button#rock-btn');
+    const paperButton = document.querySelector('button#paper-btn');
+    const scissorsButton = document.querySelector('button#scissors-btn');
+    const resetButton = document.querySelector('button#reset-btn');
 
-        if (result.startsWith('You win')) {
-            playerPoints++;
-        } else {
-            computerPoints++;
+    const playerScoreElem = document.querySelector('p#player-score');
+    const computerScoreElem = document.querySelector('p#computer-score');
+    const resultsContainerElem = document.querySelector('div#results-container');
+
+    rockButton.addEventListener('click', () => onClickHandler(
+        'rock',
+        scores,
+        playerScoreElem,
+        computerScoreElem,
+        resultsContainerElem,
+        rockButton,
+        paperButton,
+        scissorsButton
+    ));
+
+    paperButton.addEventListener('click', () => onClickHandler(
+        'paper',
+        scores,
+        playerScoreElem,
+        computerScoreElem,
+        resultsContainerElem,
+        rockButton,
+        paperButton,
+        scissorsButton
+    ));
+
+    scissorsButton.addEventListener('click', () => onClickHandler(
+        'scissors',
+        scores,
+        playerScoreElem,
+        computerScoreElem,
+        resultsContainerElem,
+        rockButton,
+        paperButton,
+        scissorsButton
+    ));
+
+    // Reset score and remove all extra result p's if they exist for reset button
+    // Also remove disabled button statuses too in case user wins/loses and clicks reset
+    resetButton.addEventListener('click', () => {
+        scores.player = 0;
+        scores.computer = 0;
+
+        playerScoreElem.textContent = `Player score: ${scores.player}`;
+        computerScoreElem.textContent = `Computer score: ${scores.computer}`;
+
+        if (document.querySelector('p#winner')) {
+            resultsContainerElem.removeChild(document.querySelector('p#winner'));
         }
 
-        console.log(`Player: ${playerPoints}, Computer: ${computerPoints}`);
-        console.log(result);
-    }
+        if (document.querySelector('p#result')) {
+            resultsContainerElem.removeChild(document.querySelector('p#result'));
+        }
 
-    console.log(playerPoints > computerPoints ? 'YOU\'RE WINNER!' : 'L BOZO');
+        rockButton.disabled = false;
+        paperButton.disabled = false;
+        scissorsButton.disabled = false;
+    })
 }
 
 game();
